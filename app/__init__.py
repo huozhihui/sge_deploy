38ddf4cacec20cc22c94d174ef69fb94f671b843
huo_mac <240516816@qq.com>
2018-04-20 18:07:25 +0800

    modify /api/v1.0 to /api/v1

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,6 @@ def create_app(config_name):
 
     from app.api import api_bp as api_blueprint
 
-    app.register_blueprint(api_blueprint, url_prefix='/api/v1.0')
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
     return app
