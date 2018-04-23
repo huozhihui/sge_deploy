c3430c5f74199ec0c7d4384064d4760ab087baf4
huo_mac <240516816@qq.com>
2018-04-23 14:57:46 +0800

    增加flask配置文件

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from config import config
 
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
     app.config['BUNDLE_ERRORS'] = True
-    app.config.update(RESTFUL_JSON=dict(ensure_ascii=False))
+    # app.config.update(RESTFUL_JSON=dict(ensure_ascii=False))
 
     from app.api import api_bp as api_blueprint
 
---
+++ config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    pass
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
