a155061d3caa5e64ee099fb5a2d26377a4a1efe5
huo_mac <240516816@qq.com>
2018-05-07 11:28:31 +0800

    修改nis部署接口结构

--- app/api/nis_api.py
+++ app/api/nis_api.py
@@ -5,28 +5,24 @@ import os
 from flask_restful import Resource
 import base
 import argument
-from app.nis.nis_centos7 import Nis
+from app.nis.nis_centos7 import NisServer, NisClient
 
 
 class NisServerApi(Resource):
     def post(self):
-        args = argument.nis_parser.parse_args(strict=True)
+        args = argument.nis_server_parser.parse_args(strict=True)
         try:
-            nis = Nis(**args)
-            nis.server(args.state)
-            msg = "nis server %s success!" % args.state
-            return base.execute_success(msg)
+            result = NisServer(**args).run()
+            return {"result": result}
         except Exception, e:
             return base.execute_fail(e)
 
 
 class NisClientApi(Resource):
     def post(self):
-        args = argument.nis_parser.parse_args(strict=True)
+        args = argument.nis_client_parser.parse_args(strict=True)
         try:
-            nis = Nis(**args)
-            nis.client(args.state)
-            msg = "nis client %s success!" % args.state
-            return base.execute_success(msg)
+            result = NisClient(**args).run()
+            return {"result": result}
         except Exception, e:
             return base.execute_fail(e)
--- app/nis/nis_centos7.py
+++ app/nis/nis_centos7.py
@@ -1,70 +1,81 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-安装部署nis服务端及客户端
-参数:
-    domain_name: 共享目录
-    nis_server_host: nis服务端IP或主机名
-    nis_client_hosts: 客户端主机信息
-        格式:'nis_client_hosts': {
-        "192.168.0.7": {
-            "float_ip": '10.61.104.20',
-            "hostname": 'nis-sge-client',
-            "username": 'root',
-            "password": 'root'
-        },
-        "192.168.0.10": {
-            "float_ip": '10.61.104.18',
-            "hostname": 'nis-sge-client',
-            "username": 'root',
-            "password": 'root'
-        },
-    }
-"""
+
 from common.ansible_task import AnsibleTask
 from common import utils, exception
 
-REQUIRE_PARAMS = ["domain_name", "nis_server_host", "nis_client_hosts"]
-DEFAULT_PARAMS = []
-
 
 class Nis():
+    def _check_params(self, require_params):
+        for params in require_params:
+            if not self.__dict__[params]:
+                raise exception.ParamsMissing(params)
+
+    def _get_extra_var(self, need_variable_names):
+        extra_var = {}
+        for params in need_variable_names:
+            extra_var[params] = self.__dict__[params]
+        return extra_var
+
+
+class NisServer(Nis):
+    """
+    安装部署nis服务端
+    参数:
+        domain_name: 共享目录
+        nis_server_host: nis服务端IP或主机名
+    """
+
     def __init__(self, **kwargs):
         self.args = utils.convert(kwargs)
         self.domain_name = self.args.get('domain_name', None)
         self.nis_server_host = self.args.get('nis_server_host', {})
-        self.nis_client_hosts = self.args.get('nis_client_hosts', {})
-        self._check_params()
 
-    def _check_params(self):
-        for params in REQUIRE_PARAMS:
-            if not self.__dict__[params]:
-                raise exception.ParamsMissing(params)
+        self.need_variable_names = ["domain_name", "nis_server_host"]
+        self.require_params = ["domain_name", "nis_server_host"]
+        self._check_params(self.require_params)
 
-    def _config_extra_var(self):
-        extra_var = {}
-        for params in REQUIRE_PARAMS:
-            extra_var[params] = self.__dict__[params]
+        self._state = self.args.get("state", "install")
+        self.task_name = "nis_server_%s" % self._state
+        self.target_hosts = [self.nis_server_host]
+        self.extra_var = self._get_extra_var(self.need_variable_names)
 
-        for params in DEFAULT_PARAMS:
-            extra_var[params] = self.__dict__[params]
+    def run(self):
+        return AnsibleTask(self.task_name, self.extra_var).api_run(self.target_hosts)
 
-        return extra_var
 
-    def _run_playbook(self, name, target_hosts, extra_var):
-        task = AnsibleTask(name, extra_var)
-        task.cmd_run(target_hosts)
+class NisClient(Nis):
+    """
+    安装部署nis客户端
+    参数:
+        domain_name: 共享目录
+        nis_server_host: nis服务端IP或主机名
+        nis_client_hosts: 客户端主机信息
+        格式:'nis_client_hosts': [
+            "192.168.0.7": {
+                "float_ip": '10.61.104.20',
+                "hostname": 'nis-sge-client',
+                "username": 'root',
+                "password": 'root'
+                },
+            ]
+    """
 
-    def server(self, way="install"):
-        task_name = "nis_server_%s" % way
-        target_hosts = self.nis_server_host
-        extra_var = self._config_extra_var()
-        self._run_playbook(task_name, target_hosts, extra_var)
+    def __init__(self, **kwargs):
+        self.args = utils.convert(kwargs)
+        self.domain_name = self.args.get('domain_name', None)
+        self.nis_server_host = self.args.get('nis_server_host', {})
+        self.nis_client_hosts = self.args.get('nis_client_hosts', [])
 
-    def client(self, way="install"):
-        task_name = "nis_client_%s" % way
-        target_hosts = self.nis_client_hosts
-        extra_var = self._config_extra_var()
-        nis_server_ip_or_hostname = self.nis_server_host.keys()[0]
-        extra_var.update({"nis_server_host": nis_server_ip_or_hostname})
-        self._run_playbook(task_name, target_hosts, extra_var)
+        self.need_variable_names = ["domain_name", "nis_server_host"]
+        self.require_params = ["domain_name", "nis_server_host", "nis_client_hosts"]
+        self._check_params(self.require_params)
+
+        self._state = self.args.get("state", "install")
+        self.task_name = "nis_client_%s" % self._state
+        self.target_hosts = self.nis_client_hosts
+        self.extra_var = self._get_extra_var(self.need_variable_names)
+        self.extra_var["nis_server_host"] = self.nis_server_host["ip"]
+
+    def run(self):
+        return AnsibleTask(self.task_name, self.extra_var).api_run(self.target_hosts)
