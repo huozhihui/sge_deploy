0395e9e1ea7ad655e9cba85a9b5c1ebe9f6acf5b
huo_mac <240516816@qq.com>
2018-05-07 11:25:09 +0800

    修改sge部署接口结构

--- app/api/argument.py
+++ app/api/argument.py
@@ -10,35 +10,28 @@ def _common_argument():
     return parser
 
 
-def _sge_argument():
+def _sge_master_argument():
     parser = _common_argument()
     parser.add_argument("sge_install_dir", type=str, default="/opt")
     parser.add_argument("sge_root_name", type=str, default="sge")
     parser.add_argument("sge_cluster_name", type=str, default="rzl")
     parser.add_argument("sge_admin_user", type=str, default="root")
     parser.add_argument("sge_master_host", type=dict, required=True)
-    parser.add_argument("queue_name", type=str)
-    parser.add_argument("nis_server_host", type=dict)
-    parser.add_argument("nfs_server_host", type=dict)
-    parser.add_argument("repo", type=str)
     return parser
 
 
 def _sge_client_argument():
-    parser = _sge_argument()
-    parser.add_argument("sge_execd_hosts", type=dict, required=True)
-    parser.remove_argument("sge_admin_user")
+    parser = _common_argument()
+    parser.add_argument("sge_install_dir", type=str, default="/opt")
+    parser.add_argument("sge_root_name", type=str, default="sge")
+    parser.add_argument("sge_cluster_name", type=str, default="rzl")
+    parser.add_argument("sge_master_host", type=dict, required=True)
+    parser.add_argument("queue_name", type=str, default="")
+    parser.add_argument("sge_execd_hosts", type=dict, action='append', required=True)
     return parser
 
 
-def _sge_cluster_argument():
-    parser = _sge_argument()
-    parser.add_argument("sge_execd_hosts", type=dict, required=True)
-    parser.add_argument("nfs_share_ip_address", type=str, required=True)
-    return parser
-
-
-def _nfs_argument():
+def _nfs_server_argument():
     parser = _common_argument()
     parser.add_argument("share_dir", type=str, required=True)
     parser.add_argument("share_ip_address", type=str, required=True)
@@ -49,22 +42,43 @@ def _nfs_argument():
 
 
 def _nfs_client_argument():
-    parser = _nfs_argument()
-    parser.add_argument("nfs_client_hosts", type=dict, required=True)
+    parser = _common_argument()
+    parser.add_argument("share_dir", type=str, required=True)
+    parser.add_argument("nfs_server_host", type=dict, required=True)
+    parser.add_argument("nfs_client_hosts", type=dict, action='append', required=True)
     return parser
 
 
-def _nis_argument():
+def _nis_server_argument():
     parser = _common_argument()
     parser.add_argument("domain_name", type=str, required=True)
     parser.add_argument("nis_server_host", type=dict, required=True)
-    parser.add_argument("nis_client_hosts", type=dict, required=True)
     return parser
 
 
-sge_parser = _sge_argument()
+def _nis_client_argument():
+    parser = _common_argument()
+    parser.add_argument("domain_name", type=str, required=True)
+    parser.add_argument("nis_server_host", type=dict, required=True)
+    parser.add_argument("nis_client_hosts", type=dict, action='append', required=True)
+    return parser
+
+
+def _repo_argument():
+    parser = reqparse.RequestParser()
+    parser.add_argument("repo", type=str, required=True)
+    parser.add_argument("target_hosts", type=dict, action='append', required=True)
+    return parser
+
+
+
+sge_master_parser = _sge_master_argument()
 sge_client_parser = _sge_client_argument()
-sge_cluster_parser = _sge_cluster_argument()
-nfs_parser = _nfs_argument()
+
+nfs_server_parser = _nfs_server_argument()
 nfs_client_parser = _nfs_client_argument()
-nis_parser = _nis_argument()
+
+nis_server_parser = _nis_server_argument()
+nis_client_parser = _nis_client_argument()
+
+repo_parser = _repo_argument()
--- app/sge/sge_centos7.py
+++ app/sge/sge_centos7.py
@@ -1,32 +1,47 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-安装部署sge master
-参数:
-    sge_install_dir: sge安装目录
-    sge_root_name: sge根名称
-    sge_cluster_name: 集群名称
-    sge_execd_hosts: 执行主机列表
-        格式:
-        sge_execd_hosts = {
-            "192.168.0.5": {
-                "float_ip": '10.61.104.111',
-                "hostname": 'nis-sge-server',
-                "username": 'root',
-                "password": 'root'
-            },
-        }
-"""
-from copy import deepcopy
 
-from common import utils, exception
 from common.ansible_task import AnsibleTask
-
-REQUIRE_PARAMS = ["sge_master_host"]
-DEFAULT_PARAMS = ["sge_install_dir", "sge_root_name", "sge_cluster_name", "sge_admin_user", "queue_name"]
+from common import utils, exception
 
 
 class Sge():
+    def _check_params(self, require_params):
+        for params in require_params:
+            if not self.__dict__[params]:
+                raise exception.ParamsMissing(params)
+
+        sge_master_host = self.__dict__["sge_master_host"]
+        sge_exec_hosts = self.__dict__.get("sge_exec_hosts", [])
+
+        for key in ["ip", "hostname"]:
+            if not sge_master_host.has_key(key):
+                raise exception.ParamsMissing("sge_master_host<%s>" % key)
+
+        for d in sge_exec_hosts:
+            for key in ["ip", "hostname"]:
+                if not d.has_key(key):
+                    raise exception.ParamsMissing("sge_execd_host<%s>" % key)
+
+    def _get_extra_var(self, need_variable_names):
+        extra_var = {}
+        for params in need_variable_names:
+            extra_var[params] = self.__dict__[params]
+
+        return extra_var
+
+
+class SgeMaster(Sge):
+    """
+    安装部署sge服务端
+    参数:
+        sge_install_dir: sge安装目录
+        sge_root_name: sge根名称
+        sge_cluster_name: 集群名称
+        sge_admin_user: sge管理用户
+        sge_master_host: sge master主机
+    """
+
     def __init__(self, **kwargs):
         self.args = utils.convert(kwargs)
         self.sge_install_dir = self.args.get('sge_install_dir', '/opt')
@@ -34,43 +49,82 @@ class Sge():
         self.sge_cluster_name = self.args.get('sge_cluster_name', 'rzl')
         self.sge_admin_user = self.args.get('sge_admin_user', 'root')
         self.sge_master_host = self.args.get('sge_master_host', {})
-        self.sge_execd_hosts = self.args.get('sge_execd_hosts', [])
-        self.queue_name = self.args.get('queue_name', "")
-        self._check_params()
-        self.extra_var = self._generate_extra_var()
 
-    def _check_params(self):
-        for params in REQUIRE_PARAMS:
-            if not self.__dict__[params]:
-                raise exception.ParamsMissing(params)
+        self.need_variable_names = ["sge_install_dir", "sge_root_name", "sge_cluster_name", "sge_admin_user"]
+        self.require_params = ["sge_master_host"]
+        self._check_params(self.require_params)
+        self._custom_check()
 
-    def _generate_extra_var(self):
-        extra_var = {}
-        for params in REQUIRE_PARAMS:
-            extra_var[params] = self.__dict__[params]
+        self._state = self.args.get("state", "install")
+        self.task_name = "sge_master_%s" % self._state
+        self.target_hosts = [self.sge_master_host]
+        self.extra_var = self._get_extra_var(self.need_variable_names)
+        self._custom_extra_var()
 
-        for params in DEFAULT_PARAMS:
-            extra_var[params] = self.__dict__[params]
-        return extra_var
-
-    def _add_extra_var(self):
-        # 判断sge_master_host和sge_execd_hosts字典参数中是否包含hostname键
-        etc_hosts = []
-        md = {}
+    def _custom_check(self):
         for key in ["ip", "hostname"]:
             if not self.sge_master_host.has_key(key):
                 raise exception.ParamsMissing("sge_master_host<%s>" % key)
-            md[key] = self.sge_master_host[key]
-        etc_hosts.append(md)
 
-        execd_hostname_list = []
+    def _custom_extra_var(self):
+        self.extra_var["etc_hosts"] = [
+            {key: self.sge_master_host[key] for key in ["ip", "hostname"]}
+        ]
+
+    def run(self):
+        return AnsibleTask(self.task_name, self.extra_var).api_run(self.target_hosts)
+
+
+class SgeClient(Sge):
+    """
+    安装部署sge客户端
+    参数:
+        sge_install_dir: sge安装目录
+        sge_root_name: sge根名称
+        sge_cluster_name: 集群名称
+        sge_master_host: sge master主机
+        sge_execd_hosts: 执行主机列表
+        queue_name: 队列名称
+    """
+
+    def __init__(self, **kwargs):
+        self.args = utils.convert(kwargs)
+        self.sge_install_dir = self.args.get('sge_install_dir', '/opt')
+        self.sge_root_name = self.args.get('sge_root_name', 'sge')
+        self.sge_cluster_name = self.args.get('sge_cluster_name', 'rzl')
+        self.sge_master_host = self.args.get('sge_master_host', {})
+        self.sge_execd_hosts = self.args.get('sge_execd_hosts', [])
+        self.queue_name = self.args.get('queue_name', "")
+
+        self.need_variable_names = ["sge_install_dir", "sge_root_name", "sge_cluster_name", "queue_name"]
+        self.require_params = ["sge_master_host", "sge_execd_hosts"]
+        self._check_params(self.require_params)
+        self._custom_check()
+
+        self._state = self.args.get("state", "install")
+        self.task_name = "sge_client_%s" % self._state
+        self.target_hosts = self.sge_execd_hosts
+        self.extra_var = self._get_extra_var(self.need_variable_names)
+        self._custom_extra_var()
+
+    def _custom_check(self):
+        for key in ["ip", "hostname"]:
+            if not self.sge_master_host.has_key(key):
+                raise exception.ParamsMissing("sge_master_host<%s>" % key)
+
         for d in self.sge_execd_hosts:
-            ed = {}
             for key in ["ip", "hostname"]:
                 if not d.has_key(key):
-                    raise exception.ParamsMissing("sge_execd_host<%s>" % key)
-                md[key] = self.sge_master_host[key]
-            etc_hosts.append(ed)
+                    raise exception.ParamsMissing("sge_execd_hosts<%s>" % key)
+
+    def _custom_extra_var(self):
+        etc_hosts = []
+        execd_hostname_list = []
+
+        etc_hosts.append({key: self.sge_master_host[key] for key in ["ip", "hostname"]})
+
+        for d in self.sge_execd_hosts:
+            etc_hosts.append({key: d[key] for key in ["ip", "hostname"]})
             execd_hostname_list.append(d["hostname"])
 
         # 增加"etc_hosts"和"sge_execd_host_list"变量
@@ -79,30 +133,10 @@ class Sge():
             "sge_execd_host_list": ",".join(execd_hostname_list)
         })
 
-    def _run_playbook(self, name, target_hosts, extra_var):
-        task = AnsibleTask(name, extra_var)
-        return task.api_run(target_hosts)
-        # task.cmd_run(target_hosts)
-        # task.run_and_retry(target_hosts)
-
-    def master(self, way="install"):
-        task_name = "sge_master_%s" % way
-        target_hosts = [self.sge_master_host]
-        self._add_extra_var()
-        return self._run_playbook(task_name, target_hosts, self.extra_var)
-
-    # 先在sge master增加计算节点,然后安装计算节点
-    def master_add_exec(self):
+    def config_master(self):
         task_name = "sge_master_add_exec"
-        target_hosts = self.sge_master_host
-        self._add_extra_var()
-        return self._run_playbook(task_name, target_hosts, self.extra_var)
+        target_hosts = [self.sge_master_host]
+        return AnsibleTask(task_name, self.extra_var).api_run(target_hosts)
 
-    def client(self, way="install"):
-        if not self.sge_execd_hosts:
-            raise exception.ParamsMissing("sge_execd_hosts")
-
-        task_name = "sge_client_%s" % way
-        target_hosts = self.sge_execd_hosts
-        self._add_extra_var()
-        return self._run_playbook(task_name, target_hosts, self.extra_var)
+    def run(self):
+        return AnsibleTask(self.task_name, self.extra_var).api_run(self.target_hosts)
