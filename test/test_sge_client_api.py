561ee14cebc174485bdc92f170a02eea2a9e2ea6
huo_mac <240516816@qq.com>
2018-05-20 13:59:45 +0800

    修改sge接口测试文件

--- test/test_sge_client_api.py
+++ test/test_sge_client_api.py
@@ -4,34 +4,34 @@ import requests
 import json
 
 url = "http://127.0.0.1:5000/api/v1/sge/client"
-#url = "http://10.42.12.160:80/api/v1/sge/client"
+# url = "http://10.42.12.160:80/api/v1/sge/client"
 params = {
     "os": "centos7",
     "state": "install",
-    'callback_url': "http://127.0.0.1:4000/sge/callback",
-    "sge_install_dir": "/opt",
-    "sge_root_name": "sge",
-    "sge_cluster_name": "rzl",
-    "queue_name": "abc",
-    "sge_master_host": {
-        "float_ip": "10.42.12.147",
+    'callbackUrl': "http://127.0.0.1:4000/sge/callback",
+    "sgeInstallDir": "/opt",
+    "sgeRootName": "sge",
+    "sgeClusterName": "rzl",
+    "queueName": "abc",
+    "sgeMasterHost": {
+        "floatIp": "10.42.12.147",
         "ip": "192.168.1.9",
-        "hostname": "sge-master",
+        # "hostname": "sge-master",
         "username": "root",
         "password": "root",
     },
-    'sge_execd_hosts': [
+    'sgeExecdHosts': [
         {
-            "float_ip": "10.42.12.157",
+            "floatIp": "10.42.12.157",
             "ip": "192.168.1.13",
-            "hostname": "sge-client-1",
+            # "hostname": "sge-client-1",
             "username": "root",
             "password": "root",
         },
         {
-            "float_ip": "10.42.12.150",
+            "floatIp": "10.42.12.150",
             "ip": "192.168.1.12",
-            "hostname": "sge-client-2",
+            # "hostname": "sge-client-2",
             "username": "root",
             "password": "root",
         },
--- test/test_sge_master_api.py
+++ test/test_sge_master_api.py
@@ -9,13 +9,13 @@ url = "http://127.0.0.1:5000/api/v1/sge/master"
 params = {
     "os": "centos7",
     "state": "install",
-    'callback_url': "http://127.0.0.1:4000/sge/callback",
-    "sge_install_dir": "/opt",
-    "sge_root_name": "sge",
-    "sge_admin_user": "root",
-    "sge_cluster_name": "rzl",
-    "sge_master_host": {
-        "float_ip": "10.42.12.147",
+    'callbackUrl': "http://127.0.0.1:4000/sge/callback",
+    "sgeInstallDir": "/opt",
+    "sgeRootName": "sge",
+    "sgeAdminUser": "root",
+    "sgeClusterName": "rzl",
+    "sgeMasterHost": {
+        "floatIp": "10.42.12.147",
         "ip": "192.168.1.9",
         "hostname": "sge-master",
         "username": "root",
