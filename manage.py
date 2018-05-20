5fd9b426c84df61846de76b66b47aa327e94a0b5
huo_mac <240516816@qq.com>
2018-05-20 13:58:31 +0800

    修改服务启动接口

--- manage.py
+++ manage.py
@@ -9,5 +9,5 @@ application = create_app(os.getenv('FLASK_CONFIG') or 'default')
 # manager = Manager(application)
 
 if __name__ == '__main__':
-    application.run(debug=True, host="0.0.0.0", port=80)
+    application.run(debug=True, host="0.0.0.0", port=5000)
     # manager.run()
