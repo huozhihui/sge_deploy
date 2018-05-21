f7bf62b71050660ae8bb862c7d2f530239c44560
huo_mac <240516816@qq.com>
2018-05-21 15:02:33 +0800

    修改启动脚本manage.py

--- manage.py
+++ manage.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from app import create_app
-# from flask_script import Manager
 import os
-
+from app import create_app
 
 application = create_app(os.getenv('FLASK_CONFIG') or 'default')
-# manager = Manager(application)
 
 if __name__ == '__main__':
     application.run(debug=True, host="0.0.0.0", port=5000)
-    # manager.run()
