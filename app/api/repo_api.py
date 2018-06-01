83458314f9ef1b437256bbc072c1abd04ae40c32
huo_mac <240516816@qq.com>
2018-06-01 10:12:22 +0800

    优化源接口，实现部署结果查询

--- app/api/repo_api.py
+++ app/api/repo_api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from flask import current_app
 from flask_restful import Resource
 import argument
 import base
@@ -8,6 +9,14 @@ from repo.repo import Repo
 
 class RepoApi(Resource):
     def post(self):
-        args = argument.repo_parser.parse_args(strict=True)
-        instance = Repo(**args)
-        base.generate_thread(instance, **args)
+        args = argument.repo_parser.parse_args()
+        task_id = base.generate_task_id()
+        log = current_app.logger
+        log.info("taskID: %s" % task_id)
+        try:
+            instance = Repo(**args)
+            base.generate_thread(task_id, instance, log)
+            return base.execute_success(task_id)
+        except Exception, e:
+            log.error(e)
+            return base.execute_fail(task_id, e)
