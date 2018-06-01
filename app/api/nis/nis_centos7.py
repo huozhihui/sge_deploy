ce72e54f89d9c97d41df3e21db86f35a7ef25b82
huo_mac <240516816@qq.com>
2018-06-01 10:11:55 +0800

    优化nis接口，实现部署结果查询

--- app/api/nis/nis_centos7.py
+++ app/api/nis/nis_centos7.py
@@ -5,17 +5,33 @@ from common import utils, exception
 from common.ansible_task import AnsibleTask
 
 
-class Nis():
-    def _check_params(self, require_params):
-        for params in require_params:
+class Nis(object):
+    def __init__(self, **kwargs):
+        self.args = utils.convert(kwargs)
+        self.domain_name = self.args.get('domainName', None)
+        self.nis_server_host = self.args.get('nisServerHost', {})
+
+        self.need_variable_names = ["domain_name", "nis_server_host"]
+        self.require_params = ["domain_name", "nis_server_host"]
+        self._state = self.args.get("state", "install")
+
+        self.task_name = "nis"
+        self.target_hosts = []
+        self.extra_var = []
+
+    def _check_params(self):
+        for params in self.require_params:
             if not self.__dict__[params]:
                 raise exception.ParamsMissing(params)
 
-    def _get_extra_var(self, need_variable_names):
+    def _get_extra_var(self):
         extra_var = {}
-        for params in need_variable_names:
+        for params in self.need_variable_names:
             extra_var[params] = self.__dict__[params]
         return extra_var
+
+    def run(self):
+        return AnsibleTask(self.task_name, self.extra_var).api_run(self.target_hosts)
 
 
 class NisServer(Nis):
@@ -27,21 +43,12 @@ class NisServer(Nis):
     """
 
     def __init__(self, **kwargs):
-        self.args = utils.convert(kwargs)
-        self.domain_name = self.args.get('domain_name', None)
-        self.nis_server_host = self.args.get('nis_server_host', {})
+        super(NisServer, self).__init__(**kwargs)
+        self._check_params()
 
-        self.need_variable_names = ["domain_name", "nis_server_host"]
-        self.require_params = ["domain_name", "nis_server_host"]
-        self._check_params(self.require_params)
-
-        self._state = self.args.get("state", "install")
         self.task_name = "nis_server_%s" % self._state
         self.target_hosts = [self.nis_server_host]
-        self.extra_var = self._get_extra_var(self.need_variable_names)
-
-    def run(self):
-        return AnsibleTask(self.task_name, self.extra_var).api_run(self.target_hosts)
+        self.extra_var = self._get_extra_var()
 
 
 class NisClient(Nis):
@@ -62,20 +69,12 @@ class NisClient(Nis):
     """
 
     def __init__(self, **kwargs):
-        self.args = utils.convert(kwargs)
-        self.domain_name = self.args.get('domain_name', None)
-        self.nis_server_host = self.args.get('nis_server_host', {})
-        self.nis_client_hosts = self.args.get('nis_client_hosts', [])
+        super(NisClient, self).__init__(**kwargs)
+        self.nis_client_hosts = self.args.get('nisClientHosts', [])
+        self.require_params.append("nis_client_hosts")
+        self._check_params()
 
-        self.need_variable_names = ["domain_name", "nis_server_host"]
-        self.require_params = ["domain_name", "nis_server_host", "nis_client_hosts"]
-        self._check_params(self.require_params)
-
-        self._state = self.args.get("state", "install")
         self.task_name = "nis_client_%s" % self._state
         self.target_hosts = self.nis_client_hosts
-        self.extra_var = self._get_extra_var(self.need_variable_names)
+        self.extra_var = self._get_extra_var()
         self.extra_var["nis_server_host"] = self.nis_server_host["ip"]
-
-    def run(self):
-        return AnsibleTask(self.task_name, self.extra_var).api_run(self.target_hosts)
--- app/api/nis_api.py
+++ app/api/nis_api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from flask import current_app
 from flask_restful import Resource
 import argument
 import base
@@ -8,13 +9,29 @@ from nis.nis_centos7 import NisServer, NisClient
 
 class NisServerApi(Resource):
     def post(self):
-        args = argument.nis_server_parser.parse_args(strict=True)
-        instance = NisServer(**args)
-        base.generate_thread(instance, **args)
+        args = argument.nis_server_parser.parse_args()
+        task_id = base.generate_task_id()
+        log = current_app.logger
+        log.info("taskID: %s" % task_id)
+        try:
+            instance = NisServer(**args)
+            base.generate_thread(task_id, instance, log)
+            return base.execute_success(task_id)
+        except Exception, e:
+            log.error(e)
+            return base.execute_fail(task_id, e)
 
 
 class NisClientApi(Resource):
     def post(self):
-        args = argument.nis_client_parser.parse_args(strict=True)
-        instance = NisClient(**args)
-        base.generate_thread(instance, **args)
+        args = argument.nis_client_parser.parse_args()
+        task_id = base.generate_task_id()
+        log = current_app.logger
+        log.info("taskID: %s" % task_id)
+        try:
+            instance = NisClient(**args)
+            base.generate_thread(task_id, instance, log)
+            return base.execute_success(task_id)
+        except Exception, e:
+            log.error(e)
+            return base.execute_fail(task_id, e)
